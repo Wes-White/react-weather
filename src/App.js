8749acb7e5c252581acfdecef2c0e978d3f510ad
Wes-White <wesleyrwhite86@gmail.com>
2020-01-09 11:58:52 -1000

    Added react-hook-form, finshed styling down to mobile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Form from "./components/Form";
-
+import { useForm } from "react-hook-form";
+import { Button } from "reactstrap";
 import "./App.css";
 import Weather from "./components/Weather";
 
 function App() {
   const APIKEY = "0faf43864ae9fb501559fd2c33a053fe";
   const [weather, setWeather] = useState([]);
+  const { register, handleSubmit, reset } = useForm();
+  const [location, setLocation] = useState(80239);
+  const onSubmit = data => {
+    setLocation(data.zipcode);
+    reset();
+  };
 
   //useEffect hook to axios fetch here
   useEffect(() => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?zip=93711,us&appid=${APIKEY}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${APIKEY}&units=imperial`
       )
       .then(res => {
         const data = res.data;
@@ -29,20 +35,32 @@ function App() {
       })
 
       .catch(err => console.log(err));
-  }, []);
+  }, [location]);
 
-  //form that fetches
+  //form
 
   return (
     <div className="App">
-      {console.log(weather)}
-      <Weather
-        temp={weather.temperature}
-        feel={weather.realFeel}
-        city={weather.city}
-        icon={weather.icon}
-        description={weather.description}
-      />
+      <h1 className="title ">Weather Guide</h1>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-container">
+            <input name="zipcode" placeholder="Zipcode" ref={register} />
+
+            <Button className="btn" color="primary" type="submit">
+              Submit
+            </Button>
+            <h5>Enter a zipcode to check the weather</h5>
+          </div>
+        </form>
+        <Weather
+          temp={weather.temperature}
+          feel={weather.realFeel}
+          city={weather.city}
+          icon={weather.icon}
+          description={weather.description}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { Card, CardText, CardBody, CardTitle } from "reactstrap";
 
 const Weather = props => {
   return (
-    <div className="container">
-      <Card>
-        <CardBody>
-          <CardTitle>
-            <h1>{props.city}</h1>
-            <img
-              className="weather-img"
-              src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}
-              alt="weather icon"
-            ></img>
-          </CardTitle>
-        </CardBody>
-
-        <p>
-          Actual Temp: {props.temp} Real Feel: {props.feel}
-        </p>
-        <p></p>
-        <CardBody>
-          <CardText>{props.description}</CardText>
-        </CardBody>
-      </Card>
+    <div className="weather">
+      <h2>{props.city}</h2>
+      <img
+        className="weather-img"
+        src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}
+        alt="weather icon"
+      ></img>
+      <p>
+        Actual Temp: {props.temp}˚F Real Feel: {props.feel}˚F
+      </p>
+      <p>{props.description}</p>
     </div>
   );
 };
